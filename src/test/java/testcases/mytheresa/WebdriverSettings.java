d696ce22e6bffd2d9bf01004c25657b386b834b8
kavi1997 <37212467+kavi1997@users.noreply.github.com>
2019-12-02 15:47:23 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -10,7 +10,7 @@ public class WebdriverSettings {
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver",chrome_options=chrome_options);
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
